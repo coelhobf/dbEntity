f50bacb3e0f8b8d43184597a4605ed800f072790
Bruno <brunocoelho@conveniar.com.br>
2017-12-18 14:56:43 -0200

    Teste da conexão com 3 exemplos

--- dbEntity/Program.cs
+++ dbEntity/Program.cs
@@ -15,14 +15,26 @@ namespace dbEntity
 
         private static void GravarUsandoEntity()
         {
-            Produto p = new Produto();
-            p.Nome = "Livro x";
-            p.Categoria = "Livros";
-            p.Preco = 10;
-
             using (var contexto = new LojaContext())
             {
-                contexto.Produtos.Add(p);
+                Produto p1 = new Produto();
+                p1.Nome = "Harry Potter e a Ordem da Fênix";
+                p1.Categoria = "Livros";
+                p1.Preco = 19.89;
+
+                Produto p2 = new Produto();
+                p2.Nome = "Senhor dos Anéis 1";
+                p2.Categoria = "Livros";
+                p2.Preco = 19.89;
+
+                Produto p3 = new Produto();
+                p3.Nome = "O Monge e o Executivo";
+                p3.Categoria = "Livros";
+                p3.Preco = 19.89;
+
+                contexto.Produtos.Add(p1);
+                contexto.Produtos.Add(p2);
+                contexto.Produtos.Add(p3);
                 contexto.SaveChanges();
             }
         }
