f3de3de382beebe6c6e51f778474b8abef19351a
Bruno <brunocoelho@conveniar.com.br>
2017-12-18 14:30:09 -0200

    Criação da classe produto

---
+++ dbEntity/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbEntity
+{
+    internal class Produto
+    {
+        public int Id { get; internal set; }
+        public string Nome { get; internal set; }
+        public string Categoria { get; internal set; }
+        public double Preco { get; internal set; }
+    }
+}
+}
