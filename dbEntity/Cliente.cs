6817c52d98b28f9ed6a3a49d1dce1646a0821bde
Bruno <brunocoelho@conveniar.com.br>
2017-12-19 09:37:49 -0200

    Classes Cliente e Endereco

---
+++ dbEntity/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbEntity
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Endereco Endereco { get; set; }
+        public Cliente() { }
+        public Cliente(string nome, Endereco endereco)
+        {
+            this.Nome = nome;
+            this.Endereco = endereco;
+        }
+    }
+}
---
+++ dbEntity/Endereco.cs
@@ -0,0 +1,20 @@
+﻿namespace dbEntity
+{
+    public class Endereco
+    {
+        public int Numero { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public Endereco() { }
+        public Endereco(int numero, string logra, string comple, string bairro, string cidade)
+        {
+            this.Numero = numero;
+            this.Logradouro = logra;
+            this.Complemento = comple;
+            this.Bairro = bairro;
+            this.Cidade = cidade;
+        }
+    }
+}
